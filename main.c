9906755fb8c9c3e593d887207344c626ba2fb34e
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-11-26 15:26:27 +0900

    1

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student{
+	int ID;
+	char name[10];
+	double grade;
+};
+
+int main(int argc, char *argv[]) {
+	struct student s1={1911233, "장예원", 4.3};
+	struct student s2={1911233, "장예원", 4.3};
+	struct student s3={1911233, "장예원", 4.3};
+	struct student s4={1911233, "장예원", 4.3};
+	struct student s5={1911233, "장예원", 4.3};
+	struct student s6={1911233, "장예원", 4.3};
+	struct student s7={1911233, "장예원", 4.3};
+	struct student s8={1911233, "장예원", 4.3};
+	struct student s9={1911233, "장예원", 4.3};
+	struct student s10={1911233, "장예원", 4.3};
+	return 0;
+}
